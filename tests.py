df6cfc791c49688ea47c24924f5e106311a9eb4a
Ivan Fedotikov <idfedotikov@gmail.com>
2025-04-13 21:26:45 +0300

    Add 9 unit-tests

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-from main import BooksCollector
+import pytest
+
+from main import *
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -22,3 +24,110 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Идиот')
+
+        assert 'Идиот' in collector.books_genre
+
+    def test_set_book_genre(self):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Идиот')
+        collector.set_book_genre('Идиот','Детективы')
+
+        assert 'Детективы' in collector.books_genre.values()
+
+    @pytest.mark.parametrize('name', ['Идиот'])
+    def test_get_book_genre(self, name):
+
+        collector = BooksCollector()
+
+        collector.add_new_book('Идиот')
+        collector.set_book_genre('Идиот','Детективы')
+
+        assert collector.get_book_genre(name)
+
+    def test_get_books_with_specific_genre(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Идиот')
+        collector.set_book_genre('Идиот', 'Детективы')
+        collector.add_new_book('Джейн Эйр')
+        collector.set_book_genre('Джейн Эйр', 'Детективы')
+
+        two_books_with_specific_genre = collector.get_books_with_specific_genre('Детективы')
+
+        assert two_books_with_specific_genre == ['Идиот','Джейн Эйр']
+
+    def test_get_books_genre(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Идиот')
+        collector.set_book_genre('Идиот', 'Детективы')
+        collector.add_new_book('Джейн Эйр')
+        collector.set_book_genre('Джейн Эйр', 'Детективы')
+
+        list_of_books = collector.get_books_genre()
+
+        assert len(list_of_books) == 2
+
+    def test_get_books_for_children(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Тачки')
+        collector.set_book_genre('Тачки', 'Мультфильмы')
+
+        assert 'Тачки' in collector.get_books_for_children()
+
+    def test_add_add_book_in_favorites(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Тачки')
+        collector.add_book_in_favorites('Тачки')
+
+        assert 'Тачки' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Тачки')
+        collector.add_book_in_favorites('Тачки')
+        collector.delete_book_from_favorites('Тачки')
+
+        assert 'Тачки' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Тачки')
+        collector.add_book_in_favorites('Тачки')
+
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
